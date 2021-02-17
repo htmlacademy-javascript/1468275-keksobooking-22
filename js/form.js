52eda012cd7cfe7137eb42754e47ae807165ccd3
Matvey Avdeev <svetoslavhorobriy@gmail.com>
2021-02-17 18:42:53 +0300

    create form

---
+++ js/form.js
@@ -0,0 +1,27 @@
+const apartamentTypeElement = document.querySelector('.house-type');
+const apartamentPriceElement = document.querySelector('.house-price');
+const timeInElement = document.querySelector('.timein');
+const timeOutElement = document.querySelector('.timeout');
+
+const APARTAMENT_PRICE = {
+  palace: 10000,
+  flat: 1000,
+  house: 5000,
+  bungalow: 0,
+};
+
+const changeMinPrice = () => {
+  const type = apartamentTypeElement.value;
+  apartamentPriceElement.placeholder = APARTAMENT_PRICE[type];
+  apartamentPriceElement.min = APARTAMENT_PRICE[type];
+};
+
+document.addEventListener('DOMContentLoaded', changeMinPrice);
+
+apartamentTypeElement.addEventListener('change', changeMinPrice);
+timeInElement.addEventListener('change', () => {
+  timeOutElement.value = timeInElement.value;
+});
+timeOutElement.addEventListener('change', () => {
+  timeInElement.value = timeOutElement.value;
+});
