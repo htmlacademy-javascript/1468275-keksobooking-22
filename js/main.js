2ea54f7d611637270eaf0b09c483a0f5d616bcce
Matvey Avdeev <svetoslavhorobriy@gmail.com>
2021-02-17 18:21:29 +0300

    Переименовал карточку офер

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import {createCard} from './popup.js';
 import {addOffers} from './data.js';
 
 const map = document.querySelector('.map__canvas');
-const adverts = addOffers();
-const cardsMarkup = createCard(adverts[0]);
+const cardOffers = addOffers();
+const cardsMarkup = createCard(cardOffers[0]);
 
 map.appendChild(cardsMarkup);
